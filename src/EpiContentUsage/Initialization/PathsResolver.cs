d210bbe59f3d483921fdb15529bf8727493dcd8e
Dariusz Halecki <dariusz.halecki@fortedigital.no>
2022-10-17 17:53:30 +0200

    Add basic dev server switch

---
+++ src/EpiContentUsage/Initialization/PathsResolver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EPiServer.Framework.Web.Resources;
+using EPiServer.ServiceLocation;
+using EPiServer.Shell;
+
+namespace Forte.EpiContentUsage.Initialization;
+
+public static class PathsResolver
+{
+    public static string ToClientResource(Type typeInModuleAssembly, string moduleRelativeClientResourcePath)
+    {
+        var isDebug = ServiceLocator.Current.GetInstance<ClientResourceOptions>().Debug;
+
+        if (isDebug) return "http://localhost:8080/react-app.js";
+
+        return Paths.ToClientResource(typeInModuleAssembly, moduleRelativeClientResourcePath);
+    }
+}
