857969117d32731bfe99ef90ffa18fdccfbfbcc5
darekhalecki <39534151+darekhalecki@users.noreply.github.com>
2022-10-19 09:27:03 +0200

    Update authorization to Content Usage (#13)
    Co-authored-by: Dariusz Halecki <dariusz.halecki@fortedigital.no>

--- src/EpiContentUsage/Api/Features/ContentType/ContentTypeController.cs
+++ src/EpiContentUsage/Api/Features/ContentType/ContentTypeController.cs
@@ -11,7 +11,7 @@ using Swashbuckle.AspNetCore.Annotations;
 namespace Forte.EpiContentUsage.Api.Features.ContentType;
 
 [ApiController]
-[Authorize(Roles = "CmsAdmins")]
+[Authorize(Policy = "episerver:defaultshellmodule")]
 [Route("[controller]")]
 public class ContentTypeController : ControllerBase
 {
--- src/EpiContentUsage/MenuProvider.cs
+++ src/EpiContentUsage/MenuProvider.cs
@@ -10,9 +10,7 @@ namespace Forte.EpiContentUsage
         public IEnumerable<MenuItem> GetMenuItems()
         {
             var url = Paths.ToResource(GetType(), "MainView");
-            var urlMenuItem1 = new UrlMenuItem("Content Usage", "/global/cms/admin/csp", url); 
-            urlMenuItem1.IsAvailable = context => true;
-            urlMenuItem1.SortIndex = 100;
+            var urlMenuItem1 = new UrlMenuItem("Content Usage", "/global/cms/forteepicontentusage", url);
 
             return new List<MenuItem>(1) { urlMenuItem1 };
         }
