5fc63dc236847de829b18207d800a1eec936cab3
Dariusz Halecki <dariusz.halecki@fortedigital.no>
2023-03-03 09:13:14 +0100

    code cleanup

--- src/EpiContentUsage/Api/Services/ContentUsageService.cs
+++ src/EpiContentUsage/Api/Services/ContentUsageService.cs
@@ -4,21 +4,21 @@ using EPiServer;
 using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.Editor;
-using EPiServer.Filters;
 using EPiServer.Web.Routing;
 
 namespace Forte.EpiContentUsage.Api.Services;
 
 public class ContentUsageService
 {
-    private readonly IContentModelUsage _contentModelUsage;
     private readonly IContentLoader _contentLoader;
-    private readonly IPublishedStateAssessor _publishedStateAssessor;
+    private readonly IContentModelUsage _contentModelUsage;
     private readonly IContentSoftLinkRepository _contentSoftLinkRepository;
+    private readonly IPublishedStateAssessor _publishedStateAssessor;
     private readonly IUrlResolver _urlResolver;
 
     public ContentUsageService(IUrlResolver urlResolver, IContentSoftLinkRepository contentSoftLinkRepository,
-        IContentModelUsage contentModelUsage, IContentLoader contentLoader, IPublishedStateAssessor publishedStateAssessor)
+        IContentModelUsage contentModelUsage, IContentLoader contentLoader,
+        IPublishedStateAssessor publishedStateAssessor)
     {
         _urlResolver = urlResolver;
         _contentSoftLinkRepository = contentSoftLinkRepository;
@@ -31,16 +31,14 @@ public class ContentUsageService
     {
         var url = _urlResolver.GetUrl(contentLink);
 
-        if (!string.IsNullOrEmpty(url))
-        {
+        if (!string.IsNullOrEmpty(url)) 
             return CheckIsPublished(contentLink) ? new[] { url } : new string[] { };
-        }
-        
+
         var pageLinks = _contentSoftLinkRepository.Load(contentLink, true)
             .Where(softLink => softLink.SoftLinkType == ReferenceType.PageLinkReference)
             .Select(softLink => softLink.OwnerContentLink)
             .Where(ownerContentLink => ownerContentLink != null && CheckIsPublished(ownerContentLink));
-        
+
         return pageLinks.Select(pageLink => _urlResolver.GetUrl(pageLink));
     }
 
@@ -64,7 +62,7 @@ public class ContentUsageService
     private bool CheckIsPublished(ContentReference contentLink)
     {
         var content = _contentLoader.Get<IContent>(contentLink);
-        
+
         return _publishedStateAssessor.IsPublished(content);
     }
 }
