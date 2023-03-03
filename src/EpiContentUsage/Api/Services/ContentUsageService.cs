37c6370f0e5109ce412ee6e77d4159e6872b1806
Dariusz Halecki <dariusz.halecki@fortedigital.no>
2023-03-03 08:28:18 +0100

    filter out unpublished content urls for visitors

--- src/EpiContentUsage/Api/Services/ContentUsageService.cs
+++ src/EpiContentUsage/Api/Services/ContentUsageService.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using EPiServer;
 using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.Editor;
+using EPiServer.Filters;
 using EPiServer.Web.Routing;
 
 namespace Forte.EpiContentUsage.Api.Services;
@@ -10,29 +12,36 @@ namespace Forte.EpiContentUsage.Api.Services;
 public class ContentUsageService
 {
     private readonly IContentModelUsage _contentModelUsage;
+    private readonly IContentLoader _contentLoader;
+    private readonly IPublishedStateAssessor _publishedStateAssessor;
     private readonly IContentSoftLinkRepository _contentSoftLinkRepository;
     private readonly IUrlResolver _urlResolver;
 
     public ContentUsageService(IUrlResolver urlResolver, IContentSoftLinkRepository contentSoftLinkRepository,
-        IContentModelUsage contentModelUsage)
+        IContentModelUsage contentModelUsage, IContentLoader contentLoader, IPublishedStateAssessor publishedStateAssessor)
     {
         _urlResolver = urlResolver;
         _contentSoftLinkRepository = contentSoftLinkRepository;
         _contentModelUsage = contentModelUsage;
+        _contentLoader = contentLoader;
+        _publishedStateAssessor = publishedStateAssessor;
     }
 
     public IEnumerable<string> GetPageUrls(ContentReference contentLink)
     {
         var url = _urlResolver.GetUrl(contentLink);
 
-        if (!string.IsNullOrEmpty(url)) return new[] { url };
-
-        var urls = _contentSoftLinkRepository.Load(contentLink, true)
-            .Where(softLink => softLink.SoftLinkType == ReferenceType.PageLinkReference &&
-                               !ContentReference.IsNullOrEmpty(softLink.OwnerContentLink))
-            .Select(softLink => _urlResolver.GetUrl(softLink.OwnerContentLink));
-
-        return urls;
+        if (!string.IsNullOrEmpty(url))
+        {
+            return CheckIsPublished(contentLink) ? new[] { url } : new string[] { };
+        }
+        
+        var pageLinks = _contentSoftLinkRepository.Load(contentLink, true)
+            .Where(softLink => softLink.SoftLinkType == ReferenceType.PageLinkReference)
+            .Select(softLink => softLink.OwnerContentLink)
+            .Where(ownerContentLink => ownerContentLink != null && CheckIsPublished(ownerContentLink));
+        
+        return pageLinks.Select(pageLink => _urlResolver.GetUrl(pageLink));
     }
 
     public string GetEditUrl(ContentUsage contentUsage)
@@ -50,5 +59,12 @@ public class ContentUsageService
             .Select(v => v.MaxBy(u => u.ContentLink.WorkID));
 
         return contentUsageLatestVersions;
+    }
+
+    private bool CheckIsPublished(ContentReference contentLink)
+    {
+        var content = _contentLoader.Get<IContent>(contentLink);
+        
+        return _publishedStateAssessor.IsPublished(content);
     }
 }
