961e4076afe1b03f0f3caf2e4a09e213aae3d0fd
Mateusz Górzański <mateusz.gorzanski@fortedigital.no>
2023-03-13 12:43:49 +0100

    format webpack config

--- src/EpiContentUsage/webpack.config.js
+++ src/EpiContentUsage/webpack.config.js
@@ -1,59 +1,61 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = (env, argv) => {
-    return {
-        entry: {
-            'epi-content-usage': './Frontend/index.tsx'
+module.exports = () => {
+  return {
+    entry: {
+      "epi-content-usage": "./Frontend/index.tsx",
+    },
+    devtool: "cheap-module-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          loader: "ts-loader",
         },
-        devtool: 'cheap-module-source-map',
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    exclude: /node_modules/,
-                    loader: 'ts-loader'
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            {
+              loader: "sass-loader",
+              options: {
+                sassOptions: {
+                  quietDeps: true,
                 },
-                {
-                  test: /\.css$/i,
-                  use: [MiniCssExtractPlugin.loader, "css-loader"],
-                },
-                {
-                    test: /\.s[ac]ss$/i,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        "css-loader",
-                        {
-                            loader: "sass-loader",
-                            options: {
-                                sassOptions: {
-                                    quietDeps: true,
-                                },
-                            },
-                        },
-                    ],
-                },
-            ]
+              },
+            },
+          ],
         },
-        output: {
-            filename: '[name].bundle.js',
-            path: path.resolve(__dirname, 'module/ClientResources')
-        },
-        resolve: {
-            extensions: ['.ts', '.tsx', '.js']
-        },
-        devServer: {
-            static: path.resolve(__dirname, 'module/ClientResources'),
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-                "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-            }
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-              filename: "[name].bundle.css",
-            }),
-        ]
-    }
+      ],
+    },
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "module/ClientResources"),
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    devServer: {
+      static: path.resolve(__dirname, "module/ClientResources"),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods":
+          "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers":
+          "X-Requested-With, content-type, Authorization",
+      },
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].bundle.css",
+      }),
+    ],
+  };
 };
