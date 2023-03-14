4694f07f022b5012b42ffd7b682aa483753a7096
KamilKranczuk <kamil.kranczuk@fortedigital.no>
2023-03-14 07:32:35 +0100

    Add base CollectionSorter abstract class

---
+++ src/EpiContentUsage/Api/Common/CollectionSorter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Forte.EpiContentUsage.Api.Common;
+
+public abstract class CollectionSorter<TSource>
+{
+    protected IEnumerable<TSource> SortBy<TKey>(IEnumerable<TSource> collection, Func<TSource, TKey> sorter, SortDirection? directions = SortDirection.Ascending)
+    {
+        return directions switch
+        {
+            SortDirection.Ascending => collection.OrderBy(sorter),
+            SortDirection.Descending => collection.OrderByDescending(sorter),
+            _ => throw new ArgumentOutOfRangeException(nameof(directions), directions, null)
+        };
+    }
+}
