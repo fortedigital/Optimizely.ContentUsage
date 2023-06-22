3c49e077591089c519ebc319d49cade2c946716e
KamilKranczuk <kamil.kranczuk@fortedigital.no>
2023-06-22 13:36:15 +0200

    Revert "Hide null type content"
    This reverts commit 6d8e1bc3988f93cc34d4f6f09f1a66279248a5e5.

--- src/EpiContentUsage/Frontend/Lib/hooks/useFilteredTableData.ts
+++ src/EpiContentUsage/Frontend/Lib/hooks/useFilteredTableData.ts
@@ -353,13 +353,14 @@ export function useFilteredTableData<TableDataType>({
   const filteredItems = useMemo(() => {
     return rows
       .filter((row) => {
-        const type = row[
-          contentTypeBaseColumnId as keyof TableDataType
-        ] as unknown;
-
-        if (type === null) return false;
-
-        if (contentTypeBases && contentTypeBaseColumnId && type) {
+        if (
+          contentTypeBases &&
+          contentTypeBaseColumnId &&
+          row[contentTypeBaseColumnId as keyof TableDataType]
+        ) {
+          const type = row[
+            contentTypeBaseColumnId as keyof TableDataType
+          ] as unknown;
           const contentTypeBase = contentTypeBases
             .filter((contentTypeBase) => contentTypeBase.visible)
             .find((contentTypeBase) => contentTypeBase.name === type);
