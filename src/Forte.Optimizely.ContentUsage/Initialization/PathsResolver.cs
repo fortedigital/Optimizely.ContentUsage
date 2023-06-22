0ffc45e9d3f7ca19d56c04d04862904181227c1f
KamilKranczuk <kamil.kranczuk@fortedigital.no>
2023-06-22 11:30:54 +0200

    Migrate project to Forte.Optimizely.ContentUsage name

---
+++ src/Forte.Optimizely.ContentUsage/Initialization/PathsResolver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using EPiServer.Shell;
+
+namespace Forte.Optimizely.ContentUsage.Initialization;
+
+public interface IPathsResolver
+{
+    string ToClientResource(Type typeInModuleAssembly, string moduleRelativeClientResourcePath);
+}
+
+public class DefaultPathsResolver : IPathsResolver
+{
+    public string ToClientResource(Type typeInModuleAssembly, string moduleRelativeClientResourcePath)
+    {
+        return Paths.ToClientResource(typeInModuleAssembly, moduleRelativeClientResourcePath);
+    }
+}
+
+public class DevServerPathsResolver : IPathsResolver
+{
+    private const string ClientResourcesRoot = "ClientResources/";
+
+    private readonly string _devServerBaseUrl;
+
+    public DevServerPathsResolver(string devServerBaseUrl)
+    {
+        _devServerBaseUrl = devServerBaseUrl;
+    }
+
+    public string ToClientResource(Type typeInModuleAssembly, string moduleRelativeClientResourcePath)
+    {
+        var isReplaced = TryReplaceAtStart(moduleRelativeClientResourcePath, ClientResourcesRoot, _devServerBaseUrl,
+            out var devServerPath);
+
+        if (!isReplaced)
+            throw new InvalidOperationException($"Unable to replace '{ClientResourcesRoot}' segment");
+
+        return devServerPath;
+    }
+
+    private static bool TryReplaceAtStart(string input, string oldValue, string newValue, out string output)
+    {
+        output = string.Empty;
+
+        if (input.StartsWith(oldValue))
+        {
+            output = newValue + input.Remove(0, oldValue.Length);
+            return true;
+        }
+
+        return false;
+    }
+}
