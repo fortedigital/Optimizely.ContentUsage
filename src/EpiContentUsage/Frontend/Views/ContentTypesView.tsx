141f1b2386bd9f9726b800d4098b677f5a95c765
KamilKranczuk <kamil.kranczuk@fortedigital.no>
2023-06-27 09:02:03 +0200

    Set fixed table min-width

--- src/EpiContentUsage/Frontend/Views/ContentTypesView.tsx
+++ src/EpiContentUsage/Frontend/Views/ContentTypesView.tsx
@@ -187,7 +187,6 @@ const ContentTypesView = () => {
               <Table
                 className="epi-content-usage-table"
                 shouldAddHover={rows.length > 0}
-                tableLayoutAlgorithm="auto"
               >
                 <Table.THead>
                   <Table.TR>
