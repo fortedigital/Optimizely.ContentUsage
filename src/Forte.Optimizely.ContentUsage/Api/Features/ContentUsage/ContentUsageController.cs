21ed218c7218ee6e7539928e49e6eb622558c0f3
Kamil Krańczuk <31856712+Fifok@users.noreply.github.com>
2023-06-29 13:00:54 +0200

    Migrate project to Forte.Optimizely.ContentUsage name (#59)
    * Migrate project to Forte.Optimizely.ContentUsage name
    
    * Give EPiServerContentUsage alias in ContentUsageService
    
    * Change epi-content-usage to forte-optimizely-content-usage

---
+++ src/Forte.Optimizely.ContentUsage/Api/Features/ContentUsage/ContentUsageController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EPiServer.DataAbstraction;
+using Forte.Optimizely.ContentUsage.Api.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Forte.Optimizely.ContentUsage.Api.Features.ContentUsage;
+
+[ApiController]
+[Authorize(Policy = "episerver:defaultshellmodule")]
+[Route("[controller]")]
+public class ContentUsageController : ControllerBase
+{
+    public const string GetContentUsagesRouteName = "ContentUsages";
+
+    private readonly IContentTypeRepository _contentTypeRepository;
+    private readonly ContentUsageService _contentUsageService;
+
+    public ContentUsageController(IContentTypeRepository contentTypeRepository, ContentUsageService contentUsageService)
+    {
+        _contentTypeRepository = contentTypeRepository;
+        _contentUsageService = contentUsageService;
+    }
+
+    [HttpGet]
+    [SwaggerResponse(StatusCodes.Status200OK, null, typeof(IEnumerable<ContentUsageDto>))]
+    [SwaggerResponse(StatusCodes.Status404NotFound)]
+    [Route("[action]", Name = GetContentUsagesRouteName)]
+    public ActionResult GetContentUsages([FromQuery] [BindRequired] Guid guid)
+    {
+        var contentType = _contentTypeRepository.Load(guid);
+
+        if (contentType == null) return NotFound();
+
+        var contentUsages = _contentUsageService.GetContentUsages(contentType);
+
+        var contentUsagesDto = contentUsages.Select(contentUsage => new ContentUsageDto
+        {
+            Id = contentUsage.ContentLink.ID,
+            ContentTypeGuid = guid,
+            Name = contentUsage.Name,
+            LanguageBranch = contentUsage.LanguageBranch,
+            PageUrls = _contentUsageService.GetPageUrls(contentUsage),
+            EditUrl = _contentUsageService.GetEditUrl(contentUsage)
+        });
+
+        return Ok(contentUsagesDto);
+    }
+}
---
+++ src/Forte.Optimizely.ContentUsage/Frontend/Lib/ContentUsageAPIClient.ts
@@ -0,0 +1,68 @@
+import axios from "axios";
+
+import { ContentTypeDto, ContentUsageDto, ContentTypeBaseDto } from "../dtos";
+
+export interface ContentUsageAPIEndpoints {
+  contentTypeBases: string | null;
+  contentType: string | null;
+  contentTypes: string | null;
+  contentUsages: string | null;
+}
+
+export interface APIResponse<ResponseSchema> {
+  data: ResponseSchema | null;
+  hasErrors: boolean;
+}
+
+export default class ContentUsageAPIClient {
+  endpoints: ContentUsageAPIEndpoints = {
+    contentTypeBases: null,
+    contentType: null,
+    contentTypes: null,
+    contentUsages: null,
+  };
+
+  public constructor(endpoints: ContentUsageAPIEndpoints) {
+    this.endpoints = endpoints;
+  }
+
+  private async get<ResponseSchema>(
+    url: string,
+    params?: Record<string, string>
+  ): Promise<APIResponse<ResponseSchema>> {
+    let response,
+      queryParams,
+      hasErrors = false;
+
+    if (params) queryParams = new URLSearchParams(params).toString();
+
+    try {
+      response = await axios.get<ResponseSchema>(
+        queryParams ? `${url}?${queryParams}` : url
+      );
+    } catch (error) {
+      hasErrors = true;
+    }
+
+    return {
+      data: response.data,
+      hasErrors,
+    } as APIResponse<ResponseSchema>;
+  }
+
+  public async getContentTypeBases() {
+    return this.get<ContentTypeBaseDto[]>(this.endpoints.contentTypeBases);
+  }
+
+  public async getContentTypes() {
+    return this.get<ContentTypeDto[]>(this.endpoints.contentTypes);
+  }
+
+  public async getContentType(guid: string) {
+    return this.get<ContentTypeDto>(this.endpoints.contentType, { guid });
+  }
+
+  public async getContentTypeUsages(guid: string) {
+    return this.get<ContentUsageDto[]>(this.endpoints.contentUsages, { guid });
+  }
+}
