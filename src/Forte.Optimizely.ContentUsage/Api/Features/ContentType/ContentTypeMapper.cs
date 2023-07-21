62793194d0d4b610c2fc0a3f3927d8429cb4941d
KamilKranczuk <kamil.kranczuk@fortedigital.no>
2023-07-21 09:27:04 +0200

    Clean ContentTypeMapper

--- src/Forte.Optimizely.ContentUsage/Api/Features/ContentType/ContentTypeMapper.cs
+++ src/Forte.Optimizely.ContentUsage/Api/Features/ContentType/ContentTypeMapper.cs
@@ -1,17 +1,8 @@
-﻿using System.Linq;
-using Forte.Optimizely.ContentUsage.Api.Services;
-
+﻿
 namespace Forte.Optimizely.ContentUsage.Api.Features.ContentType;
 
 public class ContentTypeMapper
 {
-    private readonly ContentUsageService _contentUsageService;
-    
-    public ContentTypeMapper(ContentUsageService contentUsageService)
-    {
-        _contentUsageService = contentUsageService;
-    }
-
     public ContentTypeDto Map(EPiServer.DataAbstraction.ContentType contentType)
     {
         var dto = new ContentTypeDto
