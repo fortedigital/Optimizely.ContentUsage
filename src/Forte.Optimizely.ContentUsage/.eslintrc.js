be2299809082e043eb032931ed5a35b581fb05e9
Mateusz Górzański <mateusz.gorzanski@fortedigital.no>
2024-03-13 08:33:38 +0100

    Fix eslint configuration, add additional rules

---
+++ src/Forte.Optimizely.ContentUsage/.eslintrc.js
@@ -0,0 +1,33 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:compat/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "compat"],
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      typescript: true,
+    },
+  },
+};
